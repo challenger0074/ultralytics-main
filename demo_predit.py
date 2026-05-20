e41806f915d22dc04b766ceeecbd57dcdb6cd3f1
UltralyticsAssistant <web@ultralytics.com>
2026-05-20 08:08:32 +0000

    Auto-format by https://ultralytics.com/actions

--- demo_predit.py
+++ demo_predit.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
-yolo=YOLO('./yolov8n.pt',task='detect')
-results=yolo('./ultralytics/assets/bus.jpg')
-result=results[0]
+
+yolo = YOLO("./yolov8n.pt", task="detect")
+results = yolo("./ultralytics/assets/bus.jpg")
+result = results[0]
