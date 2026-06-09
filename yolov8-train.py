caadbd6ec51a2d48ddc754b4bf1b93c384b62696
kankasen <1925517594@qq.com>
2026-06-09 16:21:10 +0800

    优化gitnore

--- yolov8-train.py
+++ yolov8-train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 # Train the model
-model.train(data='yolov8-bvn.yaml', epochs=50, imgsz=640, batch=16, workers=0)  # train the model
+model.train(data='yolov8-bvn.yaml', epochs=50, imgsz=640, batch=2, workers=0)  # train the model
